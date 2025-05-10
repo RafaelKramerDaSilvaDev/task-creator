ba994c6bcc7c5bf8a6f21efa33ee339298d3e5df
Rafael Krämer da Silva <rafaelkramerdasilva.dev@gmail.com>
2025-05-09 22:52:18 -0300

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const m8TaskUrl = 'https://app.erpm8.cloud/M82024/Home/Index"';
+
+export const loginCredentials = {
+  user: "rafael.silva@m8sistemas.com.br",
+  password: "Echamelaculpa14@",
+};
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import puppeteer from "puppeteer";
+import { loginCredentials, m8TaskUrl } from "./constants";
+
+const run = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(m8TaskUrl);
+
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const userInput = page.locator("#input-login");
+  const passwordInput = page.locator("#input-senha");
+
+  await userInput.fill(loginCredentials.user);
+  await passwordInput.fill(loginCredentials.password);
+
+  const buttonLoginSelector =
+    "body > div.--nf-login-v3-container.--nf-prod-erp > main > aside > div > section > form.login-form > div.--nfgo > button";
+  const buttonLogin = page.locator(buttonLoginSelector);
+
+  await buttonLogin.click();
+};
+
+run();
